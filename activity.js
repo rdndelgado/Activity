d196e498fd750dae4a50f576c21d2f8034f42546
Rudini Delgado <94755373+rdndelgado@users.noreply.github.com>
2023-02-28 15:54:34 +0800

    Create activity.js

---
+++ activity.js
@@ -0,0 +1,22 @@
+let sinigang
+
+let sum = 150 + 9
+
+let product = 100 * 90
+
+let active = true
+let confirm = prompt("Are you currently online? y/n:  ")
+if (confirm==='N' || confirm==='n'){
+    active = false
+}
+console.log("Online: "+active)
+
+const restaurants = ["Mcdo","Jolibee", "Kuya J", "Kuya B", "Kuya D"]
+
+let a = prompt("Enter num 1: ")
+let b = prompt("Enter num 2: ")
+
+var qoutient = Math.floor(a/b)
+console.log("The result of the division is: " + qoutient)
+
+
